38b84eea63bd39728b3022e2c27466e0eaa3f4c8
Mike Méndez <15219017+aldnoah93@users.noreply.github.com>
2024-01-16 18:46:43 -0600

    [+] Update seeding process

--- src/database/seed/seed.ts
+++ src/database/seed/seed.ts
@@ -1,27 +1,18 @@
 import "reflect-metadata";
-import { DataSource, DataSourceOptions } from "typeorm";
 import { runSeeders, SeederOptions } from "typeorm-extension";
 import { PostsFactory } from "./post/posts.factory";
 import MainSeeder from "./main.seeder";
+import { AppDataSource } from "./../data-source";
 
 
-const options: DataSourceOptions & SeederOptions = {
-    type: "postgres",
-    host: process.env.POSTGRES_HOST || 'localhost',
-    port: parseInt(`${process.env.POSTGRES_PORT || 5432}`),
-    username: process.env.POSTGRES_USER || 'postgres',
-    password: process.env.POSTGRES_PASSWORD || 'postgres',
-    database: process.env.POSTGRES_DB || 'postgres',
-    entities: ['{dist,src}/**/*.entity.{ts,js}'],
-    migrations: ['{dist,src}/database/migrations/*.{ts,js}'],
-    // additional seed config 
-    factories: [PostsFactory],
-    seeds: [MainSeeder],
-    };
+const options: SeederOptions = {
+  factories: [PostsFactory],
+  seeds: [MainSeeder],
+};
 
-const dataSource = new DataSource(options);
-
-dataSource.initialize().then(async () => {
-  await runSeeders(dataSource);
+AppDataSource.initialize().then(async (dataSource) => {
+  await dataSource.dropDatabase();
+  await dataSource.runMigrations();
+  await runSeeders(dataSource, options);
   process.exit();
 });
